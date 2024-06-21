3de943da4eecb96a27aa41a29d0e4ef67caa8ef3
Amruthan Manjula Anandan <rbiamru@gmail.com>
2024-06-21 08:06:35 -0400

    Add project files.

---
+++ PracticeProduct/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProduct
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, double price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            Stock += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            Stock -= amount;
+        }
+
+    }
+}
